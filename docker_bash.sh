e5316f4d21385d4d7557caa3a0c798d5984766cc
vergeman <alan.verga@gmail.com>
2022-12-17 14:48:34 -0600

    docker_bash.sh: change docker run shell helper to use docker-compose run config

--- docker_bash.sh
+++ docker_bash.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
-docker run -v "$(pwd)"/app:/app -it react-github-actions_app zsh
+# docker run -v "$(pwd)"/app:/app -it react-github-actions_app zsh
+
+# want to use config hierarchy setup in docker-compose.yml
+docker-compose run app zsh
